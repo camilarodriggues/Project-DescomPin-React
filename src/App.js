10e57e1a924d99f064e36c5fbd328a42a63f2013
perkles <camilaa.rodriggues17@gmail.com>
2023-07-08 14:50:57 -0300

    Add funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { HeaderPartil } from './partials/HeaderPartial/HeaderPartial';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-import { HomePage } from './pages/Home/HomePage'
+
+import { HomePage } from './pages/Home/HomePage';
+import { MinhasPastasPage } from "./pages/MinhasPastas/MinhasPastasPage";
+import { HeaderPartil } from './partials/HeaderPartial/HeaderPartial';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <HeaderPartil/>
-      <HomePage/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/minhas-pastas" element={<MinhasPastasPage />} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -3,15 +3,22 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from '../../components/card/card';
-import { ModalSavePin } from '../../Containers/modalSavePin/ModalSavePin';
+import { ModalSavePin } from '../../components/../Containers/modalSavePin/ModalSavePin';
 import { ModalCreateFolder } from '../../Containers/modalSavePin/ModalCreateFolder/ModalCreateFolder';
-
+import { Notification } from '../../components/Notifications/Notication'
 
 export const HomePage = () => {
   return (
     <div>
-      <ModalCreateFolder open={true} />
-     <Container fluid>
+      <ModalSavePin open={false} />
+      <ModalCreateFolder open={false} />
+      <Notification
+        message={'Criado com sucesso!'}
+        onClose={() => {
+          console.log('Clicou em fechar')
+        }}
+      />
+      <Container fluid>
       <Row>
         <Col xs={12} md={3}><Card title="Matemática" image="https://www.youtz.com.br/wp-content/uploads/2019/10/YOUTZ-MATEMATICA-ENEM.jpg" total={0} /></Col>
         <Col xs={12} md={3}><Card title="Curso" image="https://picosimagem.com.br/wp-content/uploads/2020/05/clinika_blog_2-1000x580.jpg" total={0} /></Col>
